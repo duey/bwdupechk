22c7173170b7b2c511cfe984e31b07bbd5236d4a
duey <Dueydotnet@hotmail.com>
2020-10-10 11:34:18 -0700

    Update bwdupechk.sh
    cleaning up and adding comments to code

--- bwdupechk.sh
+++ bwdupechk.sh
@@ -1,53 +1,44 @@
 #!/bin/bash
 
+# if you have a Bitwarden session key, you can add it here
+# will have to be updated whenever you unlock Bitwarden CLI
+#export BW_SESSION=""
 
-# bw list items | jq -c  '[.[] | {id}]'
+# the commands are a bit slow to show anything, so, just making some output to let the 
+# user know something is happening
+echo
+echo "Searching ..."
 
-# bw list items | jq 'sort_by( .login.password)'
-
-
-#sample="$(bw list items | jq 'sort_by( .login.password)'| jq -c  '[.[] | {id}]')"
-
-#bw list items | jq 'sort_by( .login.password)' | jq -c '.[] | .id'
-
-
-#bw list items | jq 'sort_by( .login.password) | .[] | .id'
-
-
-#bw list items | jq 'sort_by( .login.password) | .[] | select( .login.password != null) | .login.password'
-
-
-# initialize variables to compare previous entries
+# initialize variables to compare previous password and ID
 prevPass=""
 prevID=""
 
 # setup for loop to iterate through unique ID's in the list
-# I'm not sure what the middle .[] does, but, it makes it work somehow
-
-echo
-echo "Searching ..."
+# the jq line I got from searching online, most of it should be mostly self-explanatory
+#   - the "-r" provides "raw" output (ie. without quotes around the ID's) 
+#   - the pipes/lines ( | ) is for jq itself to migrate the data through it's filters
+#   - I'm not sure what the middle .[] does, but, it makes it work somehow
 
 for itemID in \
 $(bw list items | jq -r 'sort_by( .login.password) | .[] | select( .login.password != null) | .id')
 do
+  
+  # this is also just to show the user that something is happening
   echo "Item ID#" $itemID
 
-
-
   passwd=$(bw get password $itemID)
-#  echo $passwd
-#  echo
 
   if [ "$passwd" == "$prevPass" ]; then
-
-
+  
+    # quite dramatic!
     echo
     echo "***** Found password match! ..."
-
-# "$(bw get username $prevID)" == "$(bw get username $itemID)" 
-
-
     echo
+
+    # the following prints out the Bitwarden item details side by side, using pr
+    # $(tput cols) gets the current width of the terminal/screen and helps pr scale nicely
+    # "-m" to merge the input to show items side-by-side
+    # "-t" to omit pr's header
 
     pr -mt -W $(tput cols) \
     <(echo "Left side entry (1)" && bw get item $prevID --pretty) \
@@ -63,7 +54,7 @@ do
 
 
     echo
-    read -p "Enter 1 or 2 to delete, or enter nothing to skip ... " input
+    read -p "Enter 1 or 2 to delete, enter nothing to skip, or press Ctrl-C to exit ... " input
 
     case "$input" in
       1)
@@ -88,15 +79,3 @@ do
   prevPass=$passwd
   prevID=$itemID
 done
-
-
-
-#for row in $(echo "${sample}" | jq -r '.[] | @base64'); do
-#    _jq() {
-#     echo ${row} | base64 --decode | jq -r ${1}
-#    }
-
-#   echo $(_jq '.id')
-#done
-
-#echo "$sample" | jq '.[] | .id'
