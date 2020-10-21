e5264f1d42537f6c0eacd1c13e99444ec3829656
duey <Dueydotnet@hotmail.com>
2020-10-21 16:17:39 -0700

    Add files via upload
    adding the ability to skip through to a specified Bitwarden ID, so that the user doesn't have to go through all of the previous ID's again

--- bwdupechk.sh
+++ bwdupechk.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-# if you have a Bitwarden session key, you can add it here
-# (will have to be updated whenever you unlock Bitwarden CLI)
+# if you have a Bitwarden session key, you can add it to BW_SESSION variable
+# below (and uncomment the line, by taking out the # before "export")
+# NOTE: it will have to be updated whenever you unlock the Bitwarden CLI
 #export BW_SESSION=""
 
 echo "Welcome to bwdupechk!"
 
 echo
-echo "Checking bw's status ..."
+echo -n "Checking bw's status ..."
 
 # check if bw command is not working
 # 2>&1 to redirect stderr to stdout, to get all output in one
 # with --quiet, nothing will output if things are OK
 if [ "`bw status --quiet 2>&1`" != "" ]; then
+  echo
   echo "> bw status command failed, error is as follows:"
   bw status --quiet
   exit
 fi
 
-# the "bw status" has the ... well, status (locked or unlocked)
-# as well as the last synced time, which is important as bw
+# the "bw status" has the ... well, status (locked or unlocked, etc.)
+# as well as the last synced time, which is important as bw CLI
 # needs to be synced manually
 
 bwstatusoutput="$(bw status 2>/dev/null)"
@@ -27,6 +29,7 @@ bwstatusoutput="$(bw status 2>/dev/null)"
 bwstatus=$(echo $bwstatusoutput | jq -r '.status')
 
 if [ "$bwstatus" != "unlocked" ]; then
+  echo
   echo "> bw status is not unlocked, cannot continue"
   echo "Current bw status reported:" $bwstatus
 
@@ -37,9 +40,10 @@ if [ "$bwstatus" != "unlocked" ]; then
   esac
 
   echo "and follow bw's instructions, then re-run bwduepchk"
-  exit
+  exit 1
 fi
 
+echo
 echo "> status:" $bwstatus
 
 lastsync=$(echo $bwstatusoutput | jq -r '.lastSync')
@@ -48,28 +52,64 @@ echo
 echo "Last sync:" `date -d "$lastsync"` "(converted to your time zone)"
 echo "(if this is significantly in the past, please quit and run \"bw sync\")"
 
+# TODO:
+# add logic to check the amount of time that has passed and if it hasn't been 
+# that long ago, skip the following prompt
+
 echo
 read -p "Press Enter to continue (or press Ctrl-C to exit)"
+
+# initialize variables to compare previous password and ID in the following for loop
+prevPass=""
+prevID=""
+
+# I'm changing the BW ID list to a variable (instead of as a command at the top of the loop)
+# so I can program a way to skip through the list if specified on the command line
+
+# setup BWitemlist (the list for the loop) to iterate through unique Bitwarden ID's
+# the jq line I got from searching online, most of it should be mostly self-explanatory
+#  - the "-r" provides "raw" output (ie. without quotes around the ID's)
+#  - the pipes/lines ( | ) is for jq itself to migrate the data through it's filters
+#  - I'm not sure what the middle .[] does, but, it makes it work somehow
+
+echo
+echo -n "Getting list of ID's, please wait ..."
+
+BWitemlist=$(bw list items | jq -r 'sort_by( .login.password) | .[] | select( .login.password != null) | .id')
+
+# continue output on next line (after echo -n above)
+echo
+
+if [ "$1" != "" ] ; then
+  # santize provided paramater string, by removing anything that's not alphanumeric or a dash
+  startID=${1//[^a-zA-Z0-9\-]/}
+
+  # check if parameter provided is actually in the list of ID's
+  if [[ "$BWitemlist" != *"$startID"* ]] ; then
+    echo
+    echo "Provided ID ("$startID") not found, continuing from top of list..."
+  else
+    # cut out beginning of list until the provided ID
+    # Bash string manipulation, requires the curly brackets { }
+    # using the hash symbol ( # ), the following will cut from the beginning
+    # of the variable until the first occurance of the provided substring
+
+    BWitemlist=${BWitemlist#*$startID*}
+
+    # since the above removes the actual ID provided, add it back in
+    BWitemlist="$startID $BWitemlist"
+
+    echo
+    echo "Starting search at ID:" $startID
+  fi
+fi
 
 # the commands are a bit slow to show anything, so, just making some output to let the
 # user know something is happening
 echo
 echo "Searching for duplicate passwords ..."
 
-# initialize variables to compare previous password and ID
-prevPass=""
-prevID=""
-
-# setup for loop to iterate through unique ID's in the list
-# the jq line I got from searching online, most of it should be mostly self-explanatory
-#   - the "-r" provides "raw" output (ie. without quotes around the ID's)
-#   - the pipes/lines ( | ) is for jq itself to migrate the data through it's filters
-#   - I'm not sure what the middle .[] does, but, it makes it work somehow
-
-for itemID in \
-$(bw list items | jq -r 'sort_by( .login.password) | .[] | select( .login.password != null) | .id')
-do
-
+for itemID in $BWitemlist ; do
   # this is also just to show the user that something is happening
   echo "Item ID#" $itemID
 
