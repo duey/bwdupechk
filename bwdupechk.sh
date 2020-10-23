6c37a265cb6e1029ee031ab1a986973c35625212
duey <Dueydotnet@hotmail.com>
2020-10-22 21:38:21 -0700

    Add files via upload
    cleaned up code and added suggestions from "shellcheck" software

--- bwdupechk.sh
+++ bwdupechk.sh
@@ -5,17 +5,20 @@
 # NOTE: it will have to be updated whenever you unlock the Bitwarden CLI
 #export BW_SESSION=""
 
+
 echo "Welcome to bwdupechk!"
 
 echo
 echo -n "Checking bw's status, please wait ..."
+# in the above echo command, the -n prevents a new line from being printed
+# after the text (ie. the cursor stays on the same line)
 
 # check if bw command is not working
 # 2>&1 to redirect stderr to stdout, to get all output in one
 # with --quiet, nothing will output if things are OK
-if [ "`bw status --quiet 2>&1`" != "" ]; then
-  echo
-  echo "> bw status command failed, error is as follows:"
+if [ "$(bw status --quiet 2>&1)" != "" ]; then
+  echo -e "\n> bw status command failed, error is as follows:"
+  # in the above, the -e allows echo to interpret the \n as a new line char
   bw status --quiet
   exit
 fi
@@ -24,32 +27,31 @@ fi
 # as well as the last synced time, which is important as bw CLI
 # needs to be synced manually
 
-bwstatusoutput="$(bw status 2>/dev/null)"
+bwStatusOutput="$(bw status 2>/dev/null)"
 
-bwstatus=$(echo $bwstatusoutput | jq -r '.status')
+bwStatus=$(echo "$bwStatusOutput" | jq -r '.status')
 
-if [ "$bwstatus" != "unlocked" ]; then
-  echo
-  echo "> bw status is not unlocked, cannot continue"
-  echo "Current bw status reported:" $bwstatus
+if [ "$bwStatus" != "unlocked" ]; then
+  echo -e "\n> bw status is not unlocked, cannot continue"
+  echo "Current bw status reported: $bwStatus"
 
   echo -n "Please enter \"bw "
-  case "$bwstatus" in
+  case "$bwStatus" in
     "locked") echo -n "unlock\" " ;;
     *) echo -n "login\" or \"bw unlock\" (as required) " ;;
   esac
 
-  echo "and follow bw's instructions, then re-run bwduepchk"
+  echo "and follow bw's instructions, then run bwduepchk again"
   exit 1
 fi
 
 echo
-echo "> status:" $bwstatus
+echo "> status: $bwStatus"
 
-lastsync=$(echo $bwstatusoutput | jq -r '.lastSync')
+lastsync=$(echo "$bwStatusOutput" | jq -r '.lastSync')
 
 echo
-echo "Last sync:" `date -d "$lastsync"` "(converted to your time zone)"
+echo "Last sync: $(date -d "$lastsync") (converted to your time zone)"
 echo "(if this is significantly in the past, please quit and run \"bw sync\")"
 
 # TODO:
@@ -57,7 +59,8 @@ echo "(if this is significantly in the past, please quit and run \"bw sync\")"
 # that long ago, skip the following prompt
 
 echo
-read -p "Press Enter to continue (or press Ctrl-C to exit) >"
+read -rp "Press Enter to continue (or press Ctrl-C to exit) >"
+# apparently using -r with read stops it from interpretting backslashes
 
 # initialize variables to compare previous password and ID in the following for loop
 prevPass=""
@@ -104,10 +107,10 @@ if [ "$1" != "" ] ; then
     BWitemlist="$startID $BWitemlist"
 
     echo
-    echo "Starting search at ID:" $startID
+    echo "Starting search at ID: $startID"
   else
     echo
-    echo "Provided ID ("$startID") not found, starting from top of list."
+    echo "Provided ID ($startID) not found, starting from top of list."
   fi
 fi
 
@@ -118,29 +121,39 @@ echo "Searching for duplicate passwords ..."
 
 for itemID in $BWitemlist ; do
   # this is also just to show the user that something is happening
-  echo "Item ID:" $itemID
+  echo "Item ID: $itemID"
 
-  passwd=$(bw get password $itemID)
+  passwd=$(bw get password "$itemID")
 
   if [ "$passwd" == "$prevPass" ]; then
+
+#TODO: probably should add a test to see if the "username" is the same or not
+# perhaps the website as well, but, that's a bit more complex
+# (could maybe check diff output for "uri"?
 
     # quite dramatic!
     echo
     echo "***** Found password match!"
-    echo -n "Getting details of these items, please wait ..."
+    echo -n "Getting details of these items, please wait .."
 
-    prevItemLines=$(bw get item $prevID --pretty)
-    currItemLines=$(bw get item $itemID --pretty)
+    prevItemLines=$(bw get item "$prevID" --pretty)
+
+    # just because I can, I'll output another couple of dots
+    echo -n ".."
+    currItemLines=$(bw get item "$itemID" --pretty)
 
     echo
     echo
+
+    # $(tput cols) gets the current width of the terminal/screen and helps pr scale nicely
+
+    scrncols=$(tput cols)
 
     # the following prints out the Bitwarden item details side by side, using pr
-    # $(tput cols) gets the current width of the terminal/screen and helps pr scale nicely
     # "-m" to merge the input to show items side-by-side
     # "-t" to omit pr's header
 
-    pr -mt -W $(tput cols) \
+    pr -mt -W "$scrncols" \
     <(echo "Left side entry (1)" && echo "$prevItemLines") \
     <(echo "Right side entry (2)" && echo "$currItemLines")
 
@@ -148,22 +161,22 @@ for itemID in $BWitemlist ; do
     echo "Differences:"
     echo
 
-    diff --width=$(tput cols) --suppress-common-lines --side-by-side \
+    diff --width="$scrncols" --suppress-common-lines --side-by-side \
     <(echo "Left side entry (1)" && echo "$prevItemLines") \
     <(echo "Right side entry (2)" && echo "$currItemLines")
 
     echo
-    read -p "Enter 1 or 2 to delete, enter nothing to skip, or press Ctrl-C to exit script >" input
+    read -rp "Enter 1 or 2 to delete, enter nothing to skip, or press Ctrl-C to exit script >" input
 
     case "$input" in
       1)
-        echo "Deleting ID:" $prevID
-        bw delete item $prevID
+        echo "Deleting ID: $prevID"
+        bw delete item "$prevID"
         passwd=""
         ;;
       2)
-        echo "Deleting ID:" $itemID
-        bw delete item $itemID
+        echo "Deleting ID: $itemID"
+        bw delete item "$itemID"
         passwd=""
         ;;
       *)
